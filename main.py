f7a5e5993b891442b9942b13246b94d4ec58b5ff
apple <masta2288@gmail.com>
2021-11-14 17:14:13 +0200

    kalkul

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+if __name__ == '__main__':
+    def statistiska():
+        pirmo = float(input("Pirmo skaitlu:")
+        otra = float(input("Otra skaitlu:")
+
+
+        p = pirmo/otra
+
+        g = p*100
